c68062b4ad1c0f45100f1af7dc146ee76fd48b19
kushagra1007 <jainkushagra131@gmail.com>
2026-08-26 12:01:22 +0530

    Time: 50 ms (88.02%), Space: 23.9 MB (86.2%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -12,18 +12,8 @@ public:
         ListNode* p1 = headA;
         ListNode* p2 = headB;
         while(p1 != p2){
-            if(p1 == NULL){
-                p1 = headB;
-            }
-            else{
-                p1 = p1->next;
-            }
-            if(p2 == NULL){
-                p2 = headA;
-            }
-            else{
-                p2 = p2->next;
-            }
+            p1 = (p1 == NULL) ? headB : p1->next;
+            p2 = (p2 == NULL) ? headA : p2->next;
 
         }
         return p1;
