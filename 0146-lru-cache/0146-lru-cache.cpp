8680468f08a784f98b8ba862f7b98534ac0e9245
kushagra1007 <jainkushagra131@gmail.com>
2026-08-27 22:34:16 +0530

    Time: 21 ms (89.97%), Space: 48.1 MB (93.27%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,78 +1,97 @@
 class LRUCache {
 public:
-    class Node{
-        public:
-            int key,val;
-            Node* prev;
-            Node* next;
-            Node(int k, int v){
-                key = k;
-                val = v;
-                prev = next = NULL;
+    class Node {
+    public:
+        int key, val;
+        Node* prev;
+        Node* next;
 
-            } 
+        Node(int k, int v) {
+            key = k;
+            val = v;
+            prev = nullptr;
+            next = nullptr;
+        }
     };
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
 
-    unordered_map<int,Node*> m;
-    int Limit;
+private:
+    Node* head;
+    Node* tail;
+
+    unordered_map<int, Node*> m;
+    int capacity;
     
-    void addNode(Node* newNode){
-        Node* oldNext = head->next;
+    void addNode(Node* node) {
+        node->next = head->next;
+        node->prev = head;
 
-        head->next = newNode;
-        oldNext->prev = newNode;
-
-        newNode->next = oldNext;
-        newNode->prev = head;
+        head->next->prev = node;
+        head->next = node;
     }
 
-    void delNode(Node* oldNode) {
-        Node* oldPrev = oldNode->prev;
-        Node* oldNext = oldNode->next;
-
-        oldPrev->next = oldNext;     
-        oldNext->prev = oldPrev;
-
+    void delNode(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
     }
 
+    void moveToFront(Node* node) {
+        delNode(node);
+        addNode(node);
+    }
+
+public:
     LRUCache(int capacity) {
-        Limit = capacity;
+        this->capacity = capacity;
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
         head->next = tail;
         tail->prev = head;
     }
-    
+
     int get(int key) {
-        if(m.find(key) == m.end()){
+
+        if (m.find(key) == m.end()) {
             return -1;
         }
-        Node* ansNode = m[key];
-        int ans = ansNode->val;
 
-        m.erase(key);
-        delNode(ansNode);
+        Node* node = m[key];
 
-        addNode(ansNode);
-        m[key] = ansNode;
+        moveToFront(node);
 
-        return ans;
+        return node->val;
     }
-    
-    void put(int key, int val) {
-        if(m.find(key) != m.end()){
-            Node* oldNode = m[key];
-            delNode(oldNode);
-            m.erase(key);
+
+    void put(int key, int value) {
+
+        if (m.find(key) != m.end()) {
+
+            Node* node = m[key];
+
+            node->val = value;
+
+            moveToFront(node);
+
+            return;
         }
-        if(m.size() == Limit){
-            // delete LRU data
-            m.erase(tail->prev->key);
-            delNode(tail->prev); 
+
+        if (m.size() == capacity) {
+
+            Node* lru = tail->prev;
+
+            m.erase(lru->key);
+
+            delNode(lru);
+
+            delete lru;
         }
-        Node* newNode = new Node(key,val);
-        addNode(newNode);
-        m[key] = newNode;
+
+        Node* node = new Node(key, value);
+
+        addNode(node);
+
+        m[key] = node;
     }
 };
 
