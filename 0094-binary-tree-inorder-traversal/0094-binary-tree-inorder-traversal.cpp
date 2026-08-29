ba13921f56cdebfb7bd91607f3bdcd03c8ef0113
kushagra1007 <jainkushagra131@gmail.com>
2026-08-29 23:35:03 +0530

    Time: 0 ms (100%), Space: 11 MB (12.67%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,18 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    vector<int> result;
-    void traverse(TreeNode* node){
-        if(node == NULL) return;
-        
-        traverse(node->left);
-        result.push_back(node->val);
-        traverse(node->right);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        traverse(root);
+        vector<int> result;
+        stack<TreeNode*> st;
+
+        TreeNode* curr = root;
+
+        while (curr != NULL || !st.empty()) {
+
+            // Go as far left as possible
+            while (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            // Process the node
+            curr = st.top();
+            st.pop();
+
+            result.push_back(curr->val);
+
+            // Move to right subtree
+            curr = curr->right;
+        }
+
         return result;
     }
 };
+
