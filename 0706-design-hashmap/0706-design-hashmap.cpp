cef873641747f2b849a5c12363bde2ae11a9ad33
kushagra1007 <jainkushagra131@gmail.com>
2026-08-31 22:56:02 +0530

    Time: 22 ms (75.43%), Space: 59.3 MB (90.89%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,20 +1,76 @@
 class MyHashMap {
+private:
+    static const int SIZE = 1009;  
+    struct Node {
+        int key;
+        int value;
+        Node* next;
+        Node(int k, int v, Node* n = nullptr) {
+            key = k;
+            value = v;
+            next = n;
+        }
+    };
+
+    Node* table[SIZE] = {};
+    int hash(int key) {
+        return key % SIZE;
+    }
+
 public:
-    vector<int> mp;
     MyHashMap() {
-        mp.resize(10000001,-1);
+        
     }
-    
+
     void put(int key, int value) {
-        mp[key] = value;
+        int index = hash(key);
+        Node* curr = table[index];
+
+        while (curr) {
+            if (curr->key == key) {
+                curr->value = value;
+                return;
+            }
+            curr = curr->next;
+        }
+        table[index] = new Node(key, value, table[index]);
     }
-    
+
     int get(int key) {
-        return mp[key];
+        int index = hash(key);
+
+        Node* curr = table[index];
+
+        while (curr) {
+            if (curr->key == key) {
+                return curr->value;
+            }
+            curr = curr->next;
+        }
+
+        return -1;
     }
-    
+
     void remove(int key) {
-        mp[key] = -1;
+        int index = hash(key);
+
+        Node* curr = table[index];
+        Node* prev = nullptr;
+
+        while (curr) {
+            if (curr->key == key) {
+
+                if (prev == nullptr) {
+                    table[index] = curr->next;
+                } else {
+                    prev->next = curr->next;
+                }
+                delete curr;
+                return;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
     }
 };
 
