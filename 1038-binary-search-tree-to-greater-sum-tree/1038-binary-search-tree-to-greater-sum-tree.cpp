d4d46c1c29d2121a5ce49dbe1178beb0f4801ad7
kushagra1007 <jainkushagra131@gmail.com>
2026-09-04 18:22:20 +0530

    Time: 0 ms (100%), Space: 11.2 MB (78.64%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,35 +11,37 @@
  */
 class Solution {
 public:
-    void storeInorder(TreeNode* root, vector<int>& arr) {
-        if (root == nullptr)
-            return;
-
-        storeInorder(root->left, arr);
-        arr.push_back(root->val);
-        storeInorder(root->right, arr);
-    }
-
-    void updateTree(TreeNode* root, vector<int>& arr, int& index) {
-        if (root == nullptr)
-            return;
-
-        updateTree(root->left, arr, index);
-
-        root->val = arr[index];
-        index++;
-
-        updateTree(root->right, arr, index);
-    }
-
     TreeNode* bstToGst(TreeNode* root) {
-        vector<int> arr;
-        storeInorder(root, arr);
-        for (int i = arr.size() - 2; i >= 0; i--) {
-            arr[i] += arr[i + 1];
+        int sum = 0;
+        TreeNode* curr = root;
+
+        while (curr != nullptr) {
+            if (curr->right != nullptr) {
+                TreeNode* temp = curr->right;
+
+                while (temp->left != nullptr &&
+                       temp->left != curr) {
+                    temp = temp->left;
+                }
+                if (temp->left == nullptr) {
+                    temp->left = curr;
+                    curr = curr->right;
+                }
+                else {
+                    temp->left = nullptr;
+                    sum += curr->val;
+                    curr->val = sum;
+
+                    curr = curr->left;
+                }
+            }
+            else {
+                sum += curr->val;
+                curr->val = sum;
+
+                curr = curr->left;
+            }
         }
-        int index = 0;
-        updateTree(root, arr, index);
 
         return root;
     }
