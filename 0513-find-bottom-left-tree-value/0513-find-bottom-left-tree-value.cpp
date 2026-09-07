8bc789f7dd942e24c19ac3693d6dbc6d3ef823e3
kushagra1007 <jainkushagra131@gmail.com>
2026-09-07 16:54:29 +0530

    Time: 0 ms (100%), Space: 24.8 MB (78.21%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -24,16 +24,15 @@ public:
             for (int i = 0; i < n; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-
-                // First node of this level
-                if (i == 0)
+                if (i == 0){
                     ans = node->val;
-
-                if (node->left)
+                }
+                if (node->left){
                     q.push(node->left);
-
-                if (node->right)
+                }
+                if (node->right){
                     q.push(node->right);
+                }
             }
         }
 
