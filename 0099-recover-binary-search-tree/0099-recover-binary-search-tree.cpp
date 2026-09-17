e44d0bdf13333ee9b097c1191a4e7fe844bea5a7
kushagra1007 <jainkushagra131@gmail.com>
2026-09-17 23:15:04 +0530

    Time: 0 ms (100%), Space: 60.9 MB (25.5%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,28 +11,49 @@
  */
 class Solution {
 public:
-    TreeNode* first = NULL;
-    TreeNode* second = NULL;
-    TreeNode* prev = NULL;
-
-    void inorder(TreeNode* root) {
-        if (root == NULL)
-            return;
-
-        inorder(root->left);
-        if (prev != NULL && prev->val > root->val) {
-            
-            if (first == NULL)
-                first = prev;
-
-            second = root;
-        }
-        prev = root;
-        inorder(root->right);
-    }
-
     void recoverTree(TreeNode* root) {
-        inorder(root);
+        TreeNode* first = NULL;
+        TreeNode* second = NULL;
+        TreeNode* prev = NULL;
+        TreeNode* curr = root;
+
+        while (curr != NULL) {
+
+            if (curr->left == NULL) {
+                if (prev != NULL && prev->val > curr->val) {
+                    if (first == NULL)
+                        first = prev;
+
+                    second = curr;
+                }
+
+                prev = curr;
+                curr = curr->right;
+            }
+            else {
+                TreeNode* pred = curr->left;
+
+                while (pred->right != NULL && pred->right != curr)
+                    pred = pred->right;
+
+                if (pred->right == NULL) {
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else {
+                    pred->right = NULL;
+                    if (prev != NULL && prev->val > curr->val) {
+                        if (first == NULL)
+                            first = prev;
+
+                        second = curr;
+                    }
+
+                    prev = curr;
+                    curr = curr->right;
+                }
+            }
+        }
         swap(first->val, second->val);
     }
 };
