b8d2d8f4d0682ed050c1fefaaaea674df2012ba7
kushagra1007 <jainkushagra131@gmail.com>
2026-09-17 23:06:53 +0530

    Time: 3 ms (17.55%), Space: 61 MB (25.5%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,28 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* prev = NULL;
     TreeNode* first = NULL;
     TreeNode* second = NULL;
-    void inorder(TreeNode* root){
-        if(root == NULL){
-            return ;
-        }
+    TreeNode* prev = NULL;
+
+    void inorder(TreeNode* root) {
+        if (root == NULL)
+            return;
+
         inorder(root->left);
-        if(prev != NULL && prev->val > root->val){
-            if(first == NULL){
+        if (prev != NULL && prev->val > root->val) {
+            
+            if (first == NULL)
                 first = prev;
-            }
+
             second = root;
         }
         prev = root;
         inorder(root->right);
     }
+
     void recoverTree(TreeNode* root) {
         inorder(root);
-
-        int temp = first->val;
-        first->val = second->val;
-        second->val = temp;
+        swap(first->val, second->val);
     }
 };
