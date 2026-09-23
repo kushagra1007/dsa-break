e8d0daf0ae1a74c502102c621d8396a915c1fc11
kushagra1007 <jainkushagra131@gmail.com>
2026-09-24 00:08:04 +0530

    Time: 12 ms (12.46%), Space: 43.6 MB (5.04%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    bool dfs(int node,int parent,vector<vector<int>> &graph,vector<int> &visited){
-        visited[node] = 1;
-        for(int neighbour : graph[node]){
-            if(neighbour == parent)
-                continue;
-            if (visited[neighbour])
-                return true;
-            if (dfs(neighbour, node, graph, visited))
-                return true;
+
+    bool dfs(int src, int par, vector<vector<int>>& graph, vector<int>& vis) {
+        vis[src] = true;
+
+        for (int v : graph[src]) {
+
+            if (vis[v]) {
+                if (v != par)
+                    return true;
+            }
+            else {
+                if (dfs(v, src, graph, vis))
+                    return true;
+            }
         }
+
         return false;
     }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
         int n = edges.size();
-        vector<vector<int>> graph(n+1);
-        for(auto edge:edges){
+
+        vector<vector<int>> graph(n + 1);
+
+        for (auto edge : edges) {
+
             int u = edge[0];
             int v = edge[1];
-
             graph[u].push_back(v);
             graph[v].push_back(u);
-            vector<int> visted(n+1,0);
 
-            if(dfs(u,-1,graph,visted))
+            vector<int> vis(n + 1, false);
+            if (dfs(u, -1, graph, vis))
                 return edge;
         }
-        return{};
+
+        return {};
     }
 };
