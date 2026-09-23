4710fb7e9d65ab623804a7a7dfd3eeb4fbda0f1b
kushagra1007 <jainkushagra131@gmail.com>
2026-09-23 13:06:19 +0530

    Time: 7 ms (22.62%), Space: 11.9 MB (90.38%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -23,18 +23,31 @@ class Solution {
 public:
     unordered_map<Node*,Node*> mp;
 
-    void DFS(Node* node,Node* clone_node){
-        for(Node* n : node->neighbors){
-            if(mp.find(n) == mp.end()){
+    void BFS(queue<Node*> &que) {
+
+        while(!que.empty()) {
+
+            Node* node = que.front();
+            Node* clone_node = mp[node];
+            que.pop();
+
+            for(Node* n : node->neighbors) {
+
+                if(mp.find(n) == mp.end()) {
+
                 Node* clone = new Node(n->val);
                 mp[n] = clone;
                 clone_node->neighbors.push_back(clone);
-                DFS(n,clone);
-            } else{
+
+                // BFS(n,clone);
+                que.push(n);
+
+            } else {
                 clone_node->neighbors.push_back(mp[n]);
             }
         }
     }
+}
     Node* cloneGraph(Node* node) {
         if(node == NULL)
             return NULL;
@@ -42,7 +55,10 @@ public:
         mp.clear();
         Node *clone_node = new Node(node->val);
         mp[node] = clone_node;
-        DFS(node,clone_node);
+
+        queue<Node*> que;
+        que.push(node);
+        BFS(que);
 
         return clone_node;
     }
