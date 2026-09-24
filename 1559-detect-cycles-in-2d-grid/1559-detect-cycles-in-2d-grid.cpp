8223b31581a94b6e161623b50b9df6afed5e18d1
kushagra1007 <jainkushagra131@gmail.com>
2026-09-25 00:42:01 +0530

    Time: 161 ms (15.15%), Space: 208.7 MB (14.57%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,46 +1,65 @@
 class Solution {
 public:
-    int m,n;
+    int m, n;
+
     vector<vector<int>> directions = {{1, 0},{-1, 0},{0, -1},{0, 1}};
 
-    bool cycleDetectDFS(int r, int c, int prev_r, int prev_c,vector<vector<char>>& grid,vector<vector<bool>>& visited) {
+    bool cycleDetectBFS(int r, int c,vector<vector<char>>& grid,vector<vector<bool>>& visited) {
 
-        if (visited[r][c]) {
-            return true;
-        }
+        queue<pair<pair<int, int>, pair<int, int>>> q;
+        q.push({{r, c}, {-1, -1}});
         visited[r][c] = true;
-        for (auto &dir : directions) {
 
-            int new_r = r + dir[0];
-            int new_c = c + dir[1];
+        while (!q.empty()) {
 
-            if (new_r >= 0 && new_r < m &&
-                new_c >= 0 && new_c < n &&
-                grid[new_r][new_c] == grid[r][c]) {
-                if (new_r == prev_r && new_c == prev_c) {
-                    continue;
-                }
+            auto front = q.front();
+            q.pop();
 
-                if (cycleDetectDFS(new_r, new_c, r, c,grid, visited)) {
-                    return true;
+            int curr_r = front.first.first;
+            int curr_c = front.first.second;
+
+            int prev_r = front.second.first;
+            int prev_c = front.second.second;
+
+            for (auto &dir : directions) {
+
+                int new_r = curr_r + dir[0];
+                int new_c = curr_c + dir[1];
+
+                if (new_r >= 0 && new_r < m &&
+                    new_c >= 0 && new_c < n &&
+                    grid[new_r][new_c] == grid[curr_r][curr_c]) {
+                    if (new_r == prev_r && new_c == prev_c) {
+                        continue;
+                    }
+                    if (visited[new_r][new_c]) {
+                        return true;
+                    }
+
+                    visited[new_r][new_c] = true;
+
+                    q.push({{new_r, new_c},{curr_r, curr_c}});
                 }
             }
         }
 
         return false;
     }
+
     bool containsCycle(vector<vector<char>>& grid) {
+
         m = grid.size();
         n = grid[0].size();
 
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
 
-        for(int i=0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(!visited[i][j] && cycleDetectDFS(i,j,i,j,grid,visited)){
-                    return true;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    if (cycleDetectBFS(i, j, grid, visited)) {
+                        return true;
+                    }
                 }
-                    
             }
         }
         return false;
