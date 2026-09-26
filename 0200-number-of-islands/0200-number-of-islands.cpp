8d93b46192c5484618fc52b52f80ca346c9a78e4
kushagra1007 <jainkushagra131@gmail.com>
2026-09-26 23:33:33 +0530

    Time: 29 ms (40.52%), Space: 23.7 MB (23.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<bool>> &vis,vector<vector<char>>& grid, int n,int m){
-        if(i < 0 || j < 0 || i >= n|| j >= m || vis[i][j] || grid[i][j] != '1'){
-            return;
-        }
-        vis[i][j] = true;
-        dfs(i-1,j,vis,grid,n,m);
-        dfs(i,j+1,vis,grid,n,m);
-        dfs(i+1,j,vis,grid,n,m);
-        dfs(i,j-1,vis,grid,n,m);
 
+    void bfs(int i, int j, vector<vector<bool>>& vis,vector<vector<char>>& grid, int n, int m) {
+
+        queue<pair<int, int>> q;
+
+        q.push({i, j});
+        vis[i][j] = true;
+
+        while (!q.empty()) {
+
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            if (x - 1 >= 0 && !vis[x-1][y] && grid[x-1][y] == '1') {
+                vis[x-1][y] = true;
+                q.push({x-1, y});
+            }
+            if (y + 1 < m && !vis[x][y+1] && grid[x][y+1] == '1') {
+                vis[x][y+1] = true;
+                q.push({x, y+1});
+            }
+            if (x + 1 < n && !vis[x+1][y] && grid[x+1][y] == '1') {
+                vis[x+1][y] = true;
+                q.push({x+1, y});
+            }
+            if (y - 1 >= 0 && !vis[x][y-1] && grid[x][y-1] == '1') {
+                vis[x][y-1] = true;
+                q.push({x, y-1});
+            }
+        }
     }
+
     int numIslands(vector<vector<char>>& grid) {
         int island = 0;
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    dfs(i,j,vis,grid,n,m);
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    bfs(i, j, vis, grid, n, m);
                     island++;
                 }
             }
